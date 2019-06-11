aef578710a72106c404f63362428cb7b9520d26f
Douglas Henrique C. de Araújo <douglas.araujo@lnls.br>
2019-06-11 13:52:35 -0300

    Add EPICS_CA_MAX_ARRAY_BYTES value

--- ADPatch.sh
+++ ADPatch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sed -i 's/commonPlugins.cmd/EXAMPLE_commonPlugins.cmd/' /usr/local/epics/synApps/support/areaDetector-R3-3-1/ADSimDetector/iocs/simDetectorIOC/iocBoot/iocSimDetector/st_base.cmd
+sed -i 's/#epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "10000000")/epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "9999999999")/' /usr/local/epics/synApps/support/areaDetector-R3-3-1/ADSimDetector/iocs/simDetectorIOC/iocBoot/iocSimDetector/st_base.cmd
 chmod +x /usr/local/epics/synApps/support/areaDetector-R3-3-1/ADSimDetector/iocs/simDetectorIOC/iocBoot/iocSimDetector/st.cmd
 
